8452eb90e1b23a986582a6032577bd396d427c51
6tranov <sixtranov@yahoo.co.jp>
2022-06-11 10:50:49 +0900

    add convert function

--- src/components/TimeTable.vue
+++ src/components/TimeTable.vue
@@ -1,56 +1,58 @@
 <template>
-  <draggable
-    :list="emptyList"
-    :disabled="!enabled"
-    item-key="id"
-    @change="modifyTimeWhenMoved"
-    handle=".handle-only-this"
-    animation="350"
-    group="day"
-    style="border: solid; height: 57px"
-    id="deleteArea"
-  >
-    <template #item="{ element }">
-      <div>
-        {{ element }}
-      </div>
-    </template>
-  </draggable>
-
-  <div v-for="day in timeTable" :key="day.id">
-    {{ day.date }}
+  <div>
     <draggable
-      :list="day.contents"
+      :list="emptyListOfDeleteArea"
       :disabled="!enabled"
       item-key="id"
       @change="modifyTimeWhenMoved"
-      @start="enableDeleteArea"
-      @end="
-        disableDeleteArea();
-        clearEmptyList();
-      "
       handle=".handle-only-this"
       animation="350"
       group="day"
+      style="border: solid; height: 57px"
+      id="deleteArea"
     >
       <template #item="{ element }">
         <div>
-          <input type="time" v-model="element.time" />
-          <br />
-          <textarea
-            v-model="element.text"
-            placeholder="Write your action"
-          ></textarea>
-          <span class="handle-only-this">Drag</span>
+          {{ element }}
         </div>
       </template>
     </draggable>
-  </div>
-  <div>
-    <input type="time" v-model="lastTime" />
-  </div>
 
-  <button class="btn btn-secondary" @click="add">Add</button>
+    <div v-for="day in timeTable" :key="day.id">
+      {{ day.date }}
+      <draggable
+        :list="day.rows"
+        :disabled="!enabled"
+        item-key="id"
+        @change="modifyTimeWhenMoved"
+        @start="enableDeleteArea"
+        @end="
+          disableDeleteArea();
+          clearEmptyListOfDeleteArea();
+        "
+        handle=".handle-only-this"
+        animation="350"
+        group="day"
+      >
+        <template #item="{ element }">
+          <div>
+            <input type="time" v-model="element.time" />
+            <br />
+            <textarea
+              v-model="element.text"
+              placeholder="Write your action"
+            ></textarea>
+            <span class="handle-only-this">Drag</span>
+          </div>
+        </template>
+      </draggable>
+    </div>
+    <div>
+      <input type="time" v-model="lastTime" />
+    </div>
+
+    <button class="btn btn-secondary" @click="add">Add</button>
+  </div>
 </template>
 
 <script>
@@ -69,6 +71,128 @@ function isASameOrBeforeB(a, b) {
   return false;
 }
 
+function getConvertedTimeTable(timeTable, lastTime) {
+  var result = [];
+  //1日以上日付をまたいだ時の処理を考える。
+  for (var i = 0; i < timeTable.length; i++) {
+    var isAddedDay = false; //dayを書くかどうかのフラグ
+
+    //rowに無いactionをresultに追加するための処理
+    //前の日付が存在する場合
+    if (0 < i) {
+      //丸一日以上日付をまたいだ場合
+      //timeTableにある、ひとつ前の日付
+      var formerDate = new Date(
+        timeTable[i - 1].day.substr(0, 4),
+        timeTable[i - 1].day.substr(5, 2),
+        timeTable[i - 1].day.substr(8, 2)
+      );
+      //それを1日後にする。
+      formerDate.setDate(formerDate.getDate() + 1);
+      //for文で回している最中の日付
+      var thisDate = new Date(
+        timeTable[i].day.substr(0, 4),
+        timeTable[i].day.substr(5, 2),
+        timeTable[i].day.substr(8, 2)
+      );
+      //for文で回している最中の日付の前日まで
+      while (formerDate < thisDate) {
+        //dayは、日付にする。
+        //timeを、00:00~24:00にする。
+        const year = ("0000" + formerDate.getFullYear()).slice(-4);
+        const month = ("00" + formerDate.getMonth()).slice(-2);
+        const date = ("00" + formerDate.getDate()).slice(-2);
+        result.push({
+          day: `${year}-${month}-${date}`,
+          time: "00:00~24:00",
+          action:
+            timeTable[i - 1].rows[timeTable[i - 1].rows.length - 1].action,
+        });
+        //1日加算する。
+        formerDate.setDate(formerDate.getDate() + 1);
+      }
+
+      //rowが1つ以上で、最初のtimeが00:00でないとき
+      if (
+        timeTable[i].rows.length > 0 &&
+        timeTable[i].rows[0].time !== "00:00"
+      ) {
+        //timeを、00:00~最初のtimeにする。
+        result.push({
+          day: timeTable[i].day,
+          time: `00:00~${timeTable[i].rows[0].time}`,
+          action:
+            timeTable[i - 1].rows[timeTable[i - 1].rows.length - 1].action,
+        });
+        isAddedDay = true;
+      }
+
+      //rowが0個で、lastTimeが00:00ではないとき
+      if (timeTable[i].rows.length == 0 && lastTime !== "00:00") {
+        //timeを、00:00~lastTimeにする。
+        result.push({
+          day: timeTable[i].day,
+          time: `00:00~${lastTime}`,
+          action:
+            timeTable[i - 1].rows[timeTable[i - 1].rows.length - 1].action,
+        });
+        isAddedDay = true;
+      }
+    }
+
+    //rowに存在するactionをresultに追加するための処理
+    for (var j = 0; j < timeTable[i].rows.length; j++) {
+      //日付を記述するかどうかを判断する処理
+      var day = "";
+      if (!isAddedDay) {
+        day = timeTable[i].day;
+        isAddedDay = true;
+      }
+
+      //次のrowがあるとき
+      if (j < timeTable[i].rows.length - 1) {
+        //今のrowの時間~次のrowの時間を、timeとする。
+        result.push({
+          day: day,
+          time: `${timeTable[i].rows[j].time}~${timeTable[i].rows[j + 1].time}`,
+          action: timeTable[i].rows[j].action,
+        });
+        continue;
+      }
+
+      //次の日付があるとき
+      if (i < timeTable.length - 1) {
+        //今のrowの時間~24:00を、timeとする。
+        result.push({
+          day: day,
+          time: `${timeTable[i].rows[j].time}~24:00`,
+          action: timeTable[i].rows[j].action,
+        });
+        continue;
+      }
+
+      //最後の日付で、最後のrowのとき
+      result.push({
+        day: day,
+        time: `${timeTable[i].rows[j].time}~${lastTime}`,
+        action: timeTable[i].rows[j].action,
+      });
+    }
+  }
+
+  return result;
+}
+
+function getMDTimeTable(convertedTimeTable) {
+  var result = "";
+  result += "|日付|時間|行動|\n";
+  result += "|:-:|:-:|:-:|\n";
+  for (const row of convertedTimeTable) {
+    result += `|${row.day}|${row.time}|${row.action}|\n`;
+  }
+  return result;
+}
+
 let id = 1;
 export default {
   name: "TimeTable",
@@ -85,7 +209,7 @@ export default {
         {
           id: 0,
           date: "2023-02-01",
-          contents: [
+          rows: [
             {
               id: 0,
               time: "09:15",
@@ -106,7 +230,7 @@ export default {
         {
           id: 1,
           date: "2023-02-02",
-          contents: [
+          rows: [
             {
               id: 3,
               time: "09:15",
@@ -126,19 +250,23 @@ export default {
         },
       ],
       lastTime: "18:45",
-      emptyList: [],
+      emptyListOfDeleteArea: [],
     };
   },
   computed: {
     draggingInfo() {
       return this.dragging ? "under drag" : "";
     },
-    getMDTimeTable() {
-      var result = "";
+    getNull2() {
+      //ボツ
+      return null;
+      //var result = "";
 
-      return result;
+      //return result;
     },
-    getTimeTableToOutput() {
+    getNull() {
+      //ボツ
+      return null;
       /**
        * jの判定について共通化できる部分があるのでコーディングの際に修正をする。
        * result=[]
@@ -167,7 +295,7 @@ export default {
        *        const day = (j==0) ? days[i] : ""
        *        result.push({day:day,time:times[j]~24:00,action:actions[j]})
        *        result.push({days[i+1],time:00:00~days[i].times[0],action:actions[j]})
-       * 
+       *
        * 次の日に00:00のみのタスクとlastTimeが00:00だとバグるかもしれない。
        * あと、前の日付があった場合は、その日の最初の要素でもdays[i]とはならない。これは、j==0のときに、さらにその日の最初の要素が00:00から始まっていればdayをdays[i]にして、それ以外は""とすることで解決する。
        */
@@ -197,8 +325,8 @@ export default {
     disableDeleteArea: function () {
       document.getElementById("deleteArea").style.backgroundColor = "white";
     },
-    clearEmptyList: function () {
-      this.emptyList = [];
+    clearEmptyListOfDeleteArea: function () {
+      this.emptyListOfDeleteArea = [];
     },
   },
 };
